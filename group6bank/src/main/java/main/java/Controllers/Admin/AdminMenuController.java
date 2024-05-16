6030d78ca9856f509ab8bc9891f4dbe918c16e74
unknown <21521637@gm.uit.edu.vn>
2024-05-16 18:26:38 +0700

    add AdminMenu

---
+++ group6bank/src/main/java/main/java/Controllers/Admin/AdminMenuController.java
@@ -0,0 +1,19 @@
+package main.java.Controllers.Admin;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class AdminMenuController implements Initializable {
+    public Button create_client_btn;
+    public Button clients_btn;
+    public Button deposit_btn;
+    public Button logout_btn;
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+       
+    }
+    
+}
